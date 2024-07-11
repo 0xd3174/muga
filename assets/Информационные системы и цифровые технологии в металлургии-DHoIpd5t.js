fbdc501b83a5a28b4686de97d44178af25f5cf9f
salmonzik01 <salmonzik01@users.noreply.github.com>
2024-07-11 02:32:41 +0000

    deploy: 0b08455aa431e72396479051fbd045ff9d27ef5c

--- assets/Информационные системы и цифровые технологии в металлургии-DHoIpd5t.js
+++
@@ -1,1 +0,0 @@
-const _=[{regnum:300385,snils:"",total_mark:69,original:!1,priority:8,status:"Рейтинг",competition:"Основные места",compensation:"бюджетная основа"},{regnum:306877,snils:"",total_mark:0,original:!0,priority:4,status:"Зарегистрирован",competition:"Основные места",compensation:"бюджетная основа"}];export{_ as default};
--- assets/Строительство и архитектура-oHl1-5VP.js
+++
@@ -1,1 +0,0 @@
-const _=[{regnum:302407,snils:"13714233131",total_mark:0,original:!1,priority:1,status:"Зарегистрирован",competition:"Основные места",compensation:"бюджетная основа"},{regnum:310055,snils:"",total_mark:0,original:!1,priority:1,status:"Зарегистрирован",competition:"Основные места",compensation:"бюджетная основа"}];export{_ as default};
