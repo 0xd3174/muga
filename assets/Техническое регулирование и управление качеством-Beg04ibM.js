131a3710fcdef3da959aab60d96d94e7aec57fd0
salmonzik01 <salmonzik01@users.noreply.github.com>
2024-07-12 02:30:13 +0000

    deploy: 124c248c88fb1a76ccd57e42a73ab8c690eebbf8

--- assets/Техническое регулирование и управление качеством-Beg04ibM.js
+++
@@ -1,1 +0,0 @@
-const _=[{regnum:311796,snils:"17338939106",total_mark:10,original:!1,priority:8,status:"Зарегистрирован",competition:"Основные места",compensation:"бюджетная основа"},{regnum:313358,snils:"17681498825",total_mark:10,original:!1,priority:2,status:"Зарегистрирован",competition:"Основные места",compensation:"бюджетная основа"}];export{_ as default};
