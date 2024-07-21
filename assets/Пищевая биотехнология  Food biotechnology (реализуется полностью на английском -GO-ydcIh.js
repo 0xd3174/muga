f7aa5abdf67c42eba76ed71b1be65772fe520ee3
salmonzik01 <salmonzik01@users.noreply.github.com>
2024-07-21 00:49:29 +0000

    deploy: 124c248c88fb1a76ccd57e42a73ab8c690eebbf8

--- assets/Пищевая биотехнология  Food biotechnology (реализуется полностью на английском -GO-ydcIh.js
+++
@@ -1,1 +0,0 @@
-const _=[{regnum:299331,snils:"",total_mark:0,original:!1,priority:1,status:"Забрал документы",competition:"Основные места",compensation:"Контрактная основа"},{regnum:325896,snils:"",total_mark:0,original:!1,priority:1,status:"Зарегистрирован",competition:"Основные места",compensation:"Контрактная основа"}];export{_ as default};
