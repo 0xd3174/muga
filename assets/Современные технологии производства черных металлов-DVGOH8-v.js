518592ad8cf4f0eb2ae61cfd34e130e9fdb933a7
salmonzik01 <salmonzik01@users.noreply.github.com>
2024-07-23 00:46:37 +0000

    deploy: 124c248c88fb1a76ccd57e42a73ab8c690eebbf8

--- assets/Современные технологии производства черных металлов-DVGOH8-v.js
+++
@@ -1,1 +0,0 @@
-const _=[{regnum:309191,snils:"19657846749",total_mark:77,original:!1,priority:4,status:"Рейтинг",competition:"Основные места",compensation:"Контрактная основа"}];export{_ as default};
