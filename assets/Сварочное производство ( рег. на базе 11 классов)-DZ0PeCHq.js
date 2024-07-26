bd6727c8cf059da3a6e1ea6ca8cafc68b693cbbc
salmonzik01 <salmonzik01@users.noreply.github.com>
2024-07-26 00:48:21 +0000

    deploy: 124c248c88fb1a76ccd57e42a73ab8c690eebbf8

--- assets/Сварочное производство ( рег. на базе 11 классов)-DZ0PeCHq.js
+++
@@ -1,1 +0,0 @@
-const _=[{regnum:327838,snils:"15811432246",total_mark:0,original:!0,priority:1,status:"К зачислению",competition:"Основные места",compensation:"Бюджетная основа"}];export{_ as default};
