09c70c374c4ab359da4fd8875ef4649a6bd4c9b5
salmonzik01 <salmonzik01@users.noreply.github.com>
2024-07-28 00:51:04 +0000

    deploy: 124c248c88fb1a76ccd57e42a73ab8c690eebbf8

--- assets/Технология машиностроения (на базе 11 классов)-DNpslYTu.js
+++
@@ -1,1 +0,0 @@
-const _=[{regnum:320950,snils:"18424689502",total_mark:0,original:!0,priority:1,status:"К зачислению",competition:"Основные места",compensation:"Бюджетная основа"}];export{_ as default};
