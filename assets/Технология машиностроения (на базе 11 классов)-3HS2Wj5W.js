5dace0c6d358b83444c25177d6ba3263086c567b
salmonzik01 <salmonzik01@users.noreply.github.com>
2024-08-04 00:53:56 +0000

    deploy: 124c248c88fb1a76ccd57e42a73ab8c690eebbf8

---
+++ assets/Технология машиностроения (на базе 11 классов)-3HS2Wj5W.js
@@ -0,0 +1,1 @@
+const _=[{regnum:333473,snils:"19437054286",total_mark:0,original:!0,priority:1,status:"К зачислению",competition:"Основные места",compensation:"Бюджетная основа"}];export{_ as default};
